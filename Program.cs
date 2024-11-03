c8f59ca3f8ba06c31e8e702ef030e552444c0522
Anna Khit <annakhit02@mail.ru>
2024-11-03 12:44:48 +0300

    Initial application

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace solution15
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+        }
+    }
+}
